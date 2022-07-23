65ec0c25c2be2d8a60c60a788a4e1552dec83641
Philopateer Moheb <philopateermoheb1901202@gmail.com>
2022-07-23 17:45:37 +0200

    Add some Features

--- Footer/Footer/footer.cpp
+++ Footer/Footer/footer.cpp
@@ -1,5 +1,7 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
 #include<string>
+#include<fstream>
 using namespace std;
 
 class Footer {
@@ -10,12 +12,12 @@ public:
 	void ChooseFooter(string websiteName) {
 		cout << "Footer 1\n"
 			<< "----------------------------------\n"
-			<< "(website name) All rights reserved\n"
+			<< "(website name)  All rights reserved\n"
 			<< "-----------------------------------\n"
 			<< "Footer 2\n"
 			<< "------------------------------------\n"
 			<< "       (Footer Background Color)     \n"
-			<< "(website name) All rights reserved\n"
+			<< "(website name)  All rights reserved\n"
 			<< "Choose Footer Number: ";
 		while (1) {
 			cin >> FooterNum;
@@ -27,10 +29,10 @@ public:
 
 	}
 	void GenerateCode(string websitename) {
-		htmlCode = "<footer><div class=\"footerdiv\">";
+		htmlCode = "<br><br><footer><div class=\"footerdiv\">";
 		htmlCode += websitename;
-		htmlCode += "all rights reserved</div></footer>";
-		CssCode = "footer{margin-left:auto; margin-right:auto; div.footerdiv{position: fixed; left:0; bottom:0; width:100%;";
+		htmlCode += " all rights reserved</div></footer>";
+		CssCode = "footer{margin-left:auto; margin-right:auto; div.footerdiv{ text-align: center; left:0; bottom:0; width:100%; ";
 		if (FooterNum == 2) {
 			cout << "Footer Color: ";
 			string footerBackgroundColor;
@@ -39,10 +41,27 @@ public:
 			CssCode += footerBackgroundColor + ";";
 			cout << "Footer Text Color: ";
 			string footerTextColor;
+			cin >> footerTextColor;
 			CssCode += "color: " + footerTextColor + ";";
+			CssCode += "position: fixed;";
 		}
 		CssCode += "}";
 	}
 	string getHtml() { return htmlCode; }
 	string getCss() { return CssCode; }
 };
+int main() {
+	Footer f;
+	f.ChooseFooter("Phalphilooo");
+	FILE* html = fopen("index.html","w");
+	fprintf(html, "<html><head><title>Philopateer Moheb</title>");
+	fprintf(html, "<style>%s</style><body>", f.getCss().c_str());
+
+	for (size_t i = 0; i < 100; i++)
+	{
+		fprintf(html, "philo<br>");
+	}
+	fprintf(html, "%s</body></html>", f.getHtml().c_str());
+
+	return 0;
+}
